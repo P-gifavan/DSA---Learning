ec1017b7b9017c3b87251be194e2f9b70a010273
P-gifavan <pavan.23bce7898@vitapstudent.ac.in>
2026-07-06 18:12:27 +0530

    Time: 0 ms (100%), Space: 42.3 MB (67.03%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,15 @@
-
 class Solution {
     public boolean isHappy(int n) {
-
-        while (n != 1 && n != 4) {
-
-            int sum = 0;
-
-            while (n > 0) {
-                int digit = n % 10;
-                sum = sum + (digit * digit);
-                n = n / 10;
+        int sum = 0;
+        while(n > 9) {
+            while(n>0) {
+                int digit = n %10;
+                sum = sum + digit *digit;
+                n = n/10;
             }
-
             n = sum;
+            sum = 0;
         }
-
-        if (n == 1)
-            return true;
-        else
-            return false;
+       return n==1 || n==7;
     }
 }
